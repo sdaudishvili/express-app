5386692b58c39dc265e38f5594bc06eeb68a002f
sdaudishvili <sdaudishvili@gmail.com>
2020-01-22 18:00:15 +0400

    Add contacts page info

--- routes/admin/contactsRoute.js
+++ routes/admin/contactsRoute.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const ContactsController = require("../../controllers/ContactsController");
 
-router.get('/', function (req, res) {
-  res.render('contacts', { title: 'Hey', message: 'contact page' });
+router.get('/', async function (req, res) {
+  let contactsInfo = 'asdf';
+  await new ContactsController({
+    params: req.params,
+    query: req.query,
+    body: req.body,
+    send: (statusCode, data) => {
+      contactsInfo = data;
+    }
+  }).getContacts();
+  res.render('contacts', { title: 'Hey', message: 'contact page', contactsInfo});
 });
 
 module.exports = router;
