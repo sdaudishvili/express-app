f972fd4b5bc57768feb23c52c234e9c58fbea439
Sandro Daudishvili <sdaudishvili@gmail.com>
2020-02-02 02:34:39 +0400

    add headers

--- server.js
+++ server.js
@@ -13,6 +13,28 @@ const PORT = config.get("api.port");
 const HOST = config.get("api.host");
 const connectionString = config.get("api.connectionString");
 
+// Add headers
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 /* API Routes */
 const apiProjectsRoute = require("./routes/api/projectsRoute");
 const apiContactsRoute = require("./routes/api/contactsRoute");
