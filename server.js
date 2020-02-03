d3a4c6d865c53b21ac7a01f557ae4d6d0d9bfde6
sdaudishvili <sdaudishvili@gmail.com>
2020-02-03 18:36:37 +0400

    .

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const config = require("config");
+const cors = require('cors');
+
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb', parameterLimit: 50000 }));
+app.use(bodyParser.json({limit: '50mb', extended: true}));
+app.use(cors({origin: 'http://localhost:3000'}));
 
 app.set("view engine", "pug");
 
@@ -15,20 +18,15 @@ const connectionString = config.get("api.connectionString");
 
 // Add headers
 app.use(function(req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  // Request methods you wish to allow
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
-  // Request headers you wish to allow
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader("Access-Control-Allow-Credentials", true);
 
   // Pass to next layer of middleware
@@ -39,9 +37,11 @@ app.use(function(req, res, next) {
 const apiProjectsRoute = require("./routes/api/projectsRoute");
 const apiContactsRoute = require("./routes/api/contactsRoute");
 const apiAboutRoute = require("./routes/api/aboutRoute");
+const apiUploadRoute = require("./routes/api/uploadRoute");
 app.use("/api/projects", apiProjectsRoute);
 app.use("/api/contacts", apiContactsRoute);
 app.use("/api/about", apiAboutRoute);
+app.use("/api/upload", apiUploadRoute);
 
 /* ADMIN Routes */
 const adminIndexRoute = require("./routes/admin/indexRoute");
