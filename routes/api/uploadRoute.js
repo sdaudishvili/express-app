8fb4933ce5f1b74e183b7e5594339ecc92b66b50
Sandro Daudishvili <sdaudishvili@gmail.com>
2020-02-04 00:23:29 +0400

    .

--- routes/api/uploadRoute.js
+++ routes/api/uploadRoute.js
@@ -1,41 +1,41 @@
 const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const sha256 = require('js-sha256');
+const multer = require("multer");
+const sha256 = require("js-sha256");
 
-const fileTypes = ['jpg', 'png', 'jpeg', 'bmp'];
-
+const fileTypes = ["jpg", "png", "jpeg", "bmp"];
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'assets/images');
+  destination: function(req, file, cb) {
+    cb(null, "assets/images");
   },
-  filename: function (req, file, cb) {
-    cb(null, sha256(file.originalname) + '.' + file.originalname.split('.').pop() );
+  filename: function(req, file, cb) {
+    cb(
+      null,
+      sha256(file.originalname) + "." + file.originalname.split(".").pop()
+    );
   }
 });
 
-const upload = multer({ 
-  storage: storage,  
-  fileFilter: function (req, file, cb) {
-    if (!fileTypes.includes(file.originalname.split('.').pop())) {
-      return cb('Only images are allowed');
+const upload = multer({
+  storage: storage,
+  fileFilter: function(req, file, cb) {
+    if (!fileTypes.includes(file.originalname.split(".").pop())) {
+      return cb("Only images are allowed");
     }
     cb(null, true);
   }
-}).single('image');
+}).single("image");
 
 router.post("/", function(req, res, next) {
-  upload(req, res, function (err) {
+  upload(req, res, function(err) {
     if (err instanceof multer.MulterError) {
       return res.status(500).send(err);
     } else if (err) {
       return res.status(500).send(err);
     }
     return res.status(200).send(req.file);
-
   });
 });
-
 
 module.exports = router;
